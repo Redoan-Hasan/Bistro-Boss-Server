7f919a7bd60d17bc565901024100949ea9288bf9
Redoan Hasan <redoanhasan57@gmail.com>
2025-04-22 02:28:17 +0600

    connected to mongoDB

--- index.js
+++ index.js
@@ -27,6 +27,13 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
+
+    const menuCollection = client.db("Bistro-Boss-DB").collection("menu");
+
+
+
+
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
